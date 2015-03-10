7cb4b80aa0112a18fa7e23df16f75c1f219f5707
staticfunction <jamesroland@dwimmyfactory.com>
2015-03-10 10:02:15 +0800

    make release task output amd and commonjs

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,6 +5,9 @@ var gulp = require('gulp');
 var dts = require('dts-bundle');
 var ts = require('gulp-tsc');
 var mocha = require('gulp-mocha');
+var replace = require('gulp-replace');
+var insert = require('gulp-insert');
+var pkg = require('./package.json');
 
 var BUILD_DIR = "bin-build";
 var RELEASE_DIR = "bin-release";
@@ -12,7 +15,7 @@ var RELEASE_DIR = "bin-release";
 //TODO: automate release to commonjs, amd and umd
 
 gulp.task("compile", function() {
-    var stream = gulp.src(['src/kola.ts', 'test/**Spec.ts' ,'typings/tsd.d.ts'])
+    var stream = gulp.src(['src/kola.ts','typings/tsd.d.ts'])
         .pipe(ts({
             module: "commonjs",
             declaration: true
@@ -37,12 +40,30 @@ gulp.task('bundle', function(cb) {
     cb();
 })
 
+
 gulp.task('release', function() {
-    gulp.src(BUILD_DIR + '/kola.js')
-        .pipe(gulp.dest(RELEASE_DIR));
 
-    gulp.src(['package.json','README.md','LICENSE'])
-        .pipe(gulp.dest(RELEASE_DIR));
+    var commonjs = gulp.src('src/kola.ts')
+                    .pipe(ts({
+                        declarationFiles: true,
+                        module: 'commonjs'
+                    }));
 
-    gulp.src(BUILD_DIR + '/kola.d.ts').pipe(gulp.dest(RELEASE_DIR));
+    var amd = gulp.src('src/kola.ts')
+                    .pipe(ts({
+                        module: 'amd'
+                    }));
+
+    return merge([
+        gulp.src(['package.json','README.md','LICENSE'])
+            .pipe(gulp.dest(RELEASE_DIR)),
+        commonjs.dts
+            .pipe(gulp.dest(RELEASE_DIR))
+            .pipe(replace(/declare\s/g, ''))
+            .pipe(insert.wrap('declare module \"'+ pkg.name +'\" {\n', '\n}')),
+        commonjs.js
+            .pipe(gulp.dest(RELEASE_DIR)),
+        amd.js
+            .pipe(gulp.dest(RELEASE_DIR + '/amd'))
+    ])
 })
