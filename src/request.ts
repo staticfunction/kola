a809be185c6c5079159a2c2e5f3c2aaeac0898ce
staticfunction <j.cabresos@gmail.com>
2015-09-02 10:25:31 +0800

    foundation for request

--- src/request.ts
+++ src/request.ts
@@ -1,13 +1,25 @@
-import "reflect-metadata";
+import {Hook, Kontext} from './kontext';
 
-export interface Request<T> {
-  (payload?: T): RequestPromise;
+/**
+ * Request allows you to create a async methods that returns a promise.
+ */
+
+export interface Promise<R> {
+  then(success: (res: R) => void, error?: (err: any) => void): void;
 }
 
-export interface RequestPromise {
-  then(success: (res:any) => void, error: (err:any) => void): void;
+export interface Handler<P, R> {
+  execute(payload: P, kontext: Kontext): Promise<R>;
 }
 
-export function request<T>(name: string, handlers?: Function[] | {new(): any}) {
+export interface Request<P, R> {
+  (payload?: P): Promise<R>;
+}
 
+export function create<P, R>(handler: Handler<P, R>, kontext: Kontext): Request<P, R> {
+  let request:Request<P, R> = (payload?: P) => {
+    return handler.execute(payload, kontext);
+  }
+
+  return request;
 }
--- src/requestSpec.ts
+++ src/requestSpec.ts
@@ -1,5 +1,36 @@
+import {create, Request, Handler, Promise} from './request';
+import {KontextImpl, Kontext} from './kontext';
+import * as should from "should";
+
 describe("request specifications", () => {
-  it("makes an annotated method a request");
-  it("executes handlers when request is invoked");
+  let kontext = new KontextImpl();
+  let handler: Handler<number, string> = {
+    execute: function (payload: number, kontext: Kontext)  {      
+      //setTimeout()
+      var success = (res: string) => {
+        
+      }
+      
+      var error = (error: any) => {
+        
+      }
+      
+      
+      
+      var promise: Promise<string>;
+      
+      return promise;
+    }
+  }
+  
+  it("executes handlers when request is invoked", (done: MochaDone) => {
+    let sayHello = create<number, string>(handler, kontext);
+    
+    sayHello(1).then((res: string) => {
+      should.equal(res, "hello 1");
+    });
+    
+  });
   it("returns a promise");
 })
+
