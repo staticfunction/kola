6f00cb647eac6e9fe5775e824b723d7f8053cb0c
James Roland Cabresos <j.cabresos@gmail.com>
2015-09-26 17:14:11 +0800

    isolate app tests

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,43 @@
-import {Instance, Instances} from './instance';
+import {Kontext, KontextImpl} from './kontext';
 
 export class App<T> {
 
     initialized: boolean = false;
+    kontext: Kontext;
     opts: T = null;
 
     constructor(parent?: App<any>) {
-
+        this.kontext = parent ? new KontextImpl(parent.kontext) : new KontextImpl();
     }
 
-    initialize(opts?: T): void {
-
+    protected initialize(kontext: Kontext, opts?: T): void {
+   
     }
     
+    
     start(opts?: T): App<T> {
+        if(!this.initialized) {
+            this.initialize(this.kontext, opts);
+            this.initialized = true;
+        }
+        
+        this.kontext.start();
         this.opts = opts;
+        this.onStart();
         return this;
+    }
+    
+    protected onStart(): void {
+        
     }
 
     stop(): App<T> {
+      this.kontext.stop();
+      this.onStop();
       return this;
+    }
+    
+    protected onStop(): void {
+        
     }
 }
--- src/appSpec.ts
+++ src/appSpec.ts
@@ -1,13 +1,48 @@
 import {App} from './app';
-
-export class GreeterApp extends App<string> {
-
-}
+import * as should from 'should';
+import * as sinon from 'sinon';
 
 describe("App specifications", () => {
-  it("creates a kontext");
-  it("creates a kontext with a parent");
-  it("calls initialize only once with kontext and opts passed in");
-  it("calls onStart when start() is invoked");
-  it("calls onStop when stop() is invoked");
+  
+  var parentApp:App<string>;
+  var childApp:App<number>;
+  
+  beforeEach(() => {
+    parentApp = new App<string>();
+    childApp = new App<number>(parentApp);
+  })
+  
+  it("creates a kontext", function() {
+   should.exist(parentApp.kontext);
+   should.exist(childApp.kontext);
+  });
+  
+  it("creates a kontext with a parent", () => {
+    should.equal(childApp.kontext.parent, parentApp.kontext);
+  });
+
+  it("calls initialize only once with kontext and opts passed in", () => {
+      let initialize = sinon.spy(parentApp, 'initialize');
+      
+      parentApp.start();
+      should.ok(initialize.calledOnce);
+      
+      parentApp.stop();
+      parentApp.start();
+      should.ok(initialize.calledOnce);
+    });
+    
+    it("calls onStart when start() is invoked", () => {
+      let onStart = sinon.spy(parentApp, 'onStart');
+      
+      parentApp.start();
+      should.ok(onStart.calledOnce);
+    });
+    
+    it("calls onStop when stop() is invoked", () => {
+      let onStop = sinon.spy(parentApp, 'onStop');
+      
+      parentApp.stop();
+      should.ok(onStop.calledOnce);
+    });
 })
